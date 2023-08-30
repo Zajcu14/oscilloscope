475cac7cb1a18f261295cc2c7859c71ab81d8f1c
Pablo <mozgowiecpawel@gmail.com>
2023-08-30 22:09:06 +0200

    oscyloscope_v_1_2

--- rtl/trigger.sv
+++ rtl/trigger.sv
@@ -33,7 +33,7 @@ module trigger(
     
 /////////////////////////////////////////////////////////////////////////////////////////////////////////// 
    
-    parameter HIST_THRESHOLD = 15;
+    parameter HIST_THRESHOLD = 50;
     parameter ATTITUDE_LEVEL_TRIGGER = 8;
     logic [11:0] counter;
     //reg [11:0] buffer [0:0]; 
@@ -65,7 +65,8 @@ module trigger(
                 read <= 1'b0;
             end
             3'd1: begin
-                trigger_level_case <= (data_input >= LEVEL_TRIGGER - ATTITUDE_LEVEL_TRIGGER)? 3'd2 : 3'd0;
+                trigger_level_case <= (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 
+                3'd2 : ((data_input >= LEVEL_TRIGGER - ATTITUDE_LEVEL_TRIGGER)? 3'd1 : 3'd0);
                 read <= 1'b0;
             end
             3'd2: begin
@@ -73,19 +74,11 @@ module trigger(
                 read <= 1'b0;
             end
             3'd3: begin
-                trigger_level_case <= (data_input <= LEVEL_TRIGGER - ATTITUDE_LEVEL_TRIGGER)? 3'd4 : 3'd0;
+                trigger_level_case <= (data_input <= LEVEL_TRIGGER - HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)?
+                 3'd4 : ((data_input <= LEVEL_TRIGGER - ATTITUDE_LEVEL_TRIGGER)? 3'd3 : 3'd0);
                 read <= 1'b0;
-            end
+            end            
             3'd4: begin
-                trigger_level_case <= (data_input >= LEVEL_TRIGGER - ATTITUDE_LEVEL_TRIGGER)? 3'd5 : 3'd4;
-                read <= 1'b0;
-            end
-             3'd5: begin
-                trigger_level_case <= (data_input >= LEVEL_TRIGGER - ATTITUDE_LEVEL_TRIGGER)? 3'd6 : 3'd0;
-                read <= 1'b0;
-            end
-             
-            3'd6: begin
                 if(counter == 12'd512)begin
                     read <= 1'b1;
 					counter <= 0;
@@ -94,7 +87,7 @@ module trigger(
 				    trigger_buffer[counter] <= data_input;
 			//		trigger_buffer[0:511] <= {trigger_buffer[1:511],buffer[0]};
 					counter <= counter + 1 ;
-					trigger_level_case <= 3'd6;
+					trigger_level_case <= 3'd4;
 					read <= 1'b0;
 				end
 		      end
@@ -112,115 +105,3 @@ module trigger(
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////            
     
 endmodule
-
- /*   always_ff @(posedge clk) begin
-        if (rst) begin
-            // Sygna³ resetu aktywny - zresetuj stan wyjœæ triggerów i indeksów buforów
-            trigger_buffer[0] <= 'bz;
-            trigger_index <= 'b0;
-            trigger_level_case <= 'b0;
-            counter_clk <= 'b0;
-        end else begin
-            counter_clk <= counter_clk_nxt;
-            trigger_level_case <= trigger_level_case_nxt;
-            if (trigger_active) begin
-                trigger_buffer[trigger_index] <= data_input;
-                trigger_index <= trigger_index + 1;
-            end else begin
-                trigger_buffer[0] <= 12'bz;
-                trigger_index <= 8'b0;
-            end 
-        end
-    end
-    
-always_comb begin
-    counter_clk_nxt = counter_clk + 1; 
-    trigger_level_case_nxt = trigger_level_case;
-    trigger_active = 0;
-    if (counter_clk >= 555) begin
-        counter_clk_nxt = '0;
-        case (mode)
-            2'b01: begin            // Trigger aktywowany poziomem
-                case (trigger_level_case) 
-                    2'b00: begin //wait for LEVEL_TRIGGER
-                        trigger_level_case_nxt = (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b01 : 2'b00;
-                        trigger_active = 0;
-                    end  
-                    2'b01: begin //earlier period
-                        trigger_level_case_nxt = (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b10 : 2'b01;
-                        trigger_active = 0;
-
-                    end 
-                    2'b10: begin //trigger idle
-                        trigger_level_case_nxt = (data_input <= LEVEL_TRIGGER - HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b11 : 2'b10;
-                        trigger_active = 1;
-                    end
-                    2'b11: begin //trigger idle
-                        trigger_level_case_nxt = (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b01 : 2'b11;
-                        trigger_active = 1;
-                    end
-                endcase
-            end
-        
-        endcase
-end
-end
-*/
-    // Monitorowanie wejœcia i triggerowanie
-    /*always_ff @(posedge clk) begin
-        if (rst) begin
-            // Sygna³ resetu aktywny - zresetuj stan wyjœæ triggerów i indeksów buforów
-            for (int i = 0; i < 256; i++) begin
-                trigger_buffer[i] <= 12'bz;
-            end
-            trigger_index <= 8'b0;
-            trigger_level_case <= 'b0;
-        end else begin
-            trigger_level_case <= trigger_level_case_nxt;
-
-            if (trigger_active) begin
-                trigger_buffer[trigger_index] <= data_input;
-                trigger_index <= trigger_index + 1;
-            end else begin
-                 for (int i = 0; i < 256; i++) begin
-                trigger_buffer[i] <= 12'bz;
-                end
-                trigger_index <= 8'b0;
-            end 
-            
-        end
-            // Zapisz bie¿¹ce wartoœci jako poprzednie do kolejnej iteracji
-         //   prev_data_input <= data_input;
-    end
-    
-always_comb begin
-    trigger_level_case_nxt = trigger_level_case;
-    trigger_active = 0;
-    case (mode)
-
-
-        2'b01: begin            // Trigger aktywowany poziomem
-            case (trigger_level_case) 
-                2'b00: begin //wait for LEVEL_TRIGGER
-                    trigger_level_case_nxt = (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b01 : 2'b00;
-                    trigger_active = 0;
-                end  
-                2'b01: begin //earlier period
-                    trigger_level_case_nxt = (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b10 : 2'b01;
-                    trigger_active = 0;
-
-                end 
-                2'b10: begin //trigger idle
-                    trigger_level_case_nxt = (data_input <= LEVEL_TRIGGER - HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b11 : 2'b10;
-                    trigger_active = 1;
-                end
-                2'b11: begin //trigger idle
-                    trigger_level_case_nxt = (data_input >= LEVEL_TRIGGER + HIST_THRESHOLD - ATTITUDE_LEVEL_TRIGGER)? 2'b01 : 2'b11;
-                    trigger_active = 1;
-                end
-            endcase
-        end
-    endcase
-end
-
-*/
